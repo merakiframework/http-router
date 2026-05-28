5748e20c315f3e04033ca732eeadbc88ad8c3ea9
Nathan Bishop <nbish11@hotmail.com>
2026-05-28 13:29:46 +1000

    fix: wrong url reflection for 'static' routes

--- examples/Http/Users/Profile/GetAction.php
+++ examples/Http/Users/Profile/GetAction.php
@@ -9,6 +9,8 @@ final class GetAction
 {
 	public function __invoke(int|string $id)
 	{
-		return new TextResponse('GET /users/' . $id . '/profile');
+		// intentionally showing that this handler is not a resource handler as it does not use the GetAll / GetOne convention.
+		// This is to demonstrate that the router can handle non-resource routes without any special configuration.
+		return new TextResponse('GET /users/profile/' . $id);
 	}
 }
