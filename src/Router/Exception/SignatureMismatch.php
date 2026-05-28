c4232c27d9a7005687317cb9ac527ec0f0941b80
Nathan Bishop <nbish11@hotmail.com>
2026-05-28 15:35:31 +1000

    refactor: remove unused factory methods

--- src/Router/Exception/SignatureMismatch.php
+++ src/Router/Exception/SignatureMismatch.php
@@ -5,29 +5,10 @@ namespace Meraki\Http\Router\Exception;
 
 use Meraki\Http\Router\Exception;
 use Meraki\Http\Route;
-use Meraki\Http\RouteParameter;
-use Meraki\Http\Router\Exception\InvalidArgument;
 use RuntimeException;
 
 final class SignatureMismatch extends RuntimeException implements Exception
 {
-	/**
-	 * @psalm-mutation-free
-	 */
-	public static function incorrectTypes(Route $pRoute, Route $cRoute, RouteParameter $parentParam, RouteParameter $childParam): self
-	{
-		$template = 'Route "%s" must have the same type signature as parent resource "%s": expected type[s] "%s" at position %d, got "%s".';
-
-		return new self(sprintf(
-			$template,
-			$cRoute->toStringWithoutParameters(),
-			$pRoute->toStringWithoutParameters(),
-			$parentParam->typesAsString(),
-			$parentParam->position,
-			$childParam->typesAsString()
-		));
-	}
-
 	/**
 	 * Thrown when a RESTful Item/Collection handler exists at a nested
 	 * namespace but its parent collection wasn't addressed in the URL — i.e.
@@ -43,22 +24,6 @@ final class SignatureMismatch extends RuntimeException implements Exception
 
 		return new self(sprintf(
 			$template,
-			$childRoute->requestHandler
-		));
-	}
-
-	/**
-	 * @psalm-pure
-	 */
-	public static function missingRequiredParameter(Route $parentRoute, Route $childRoute, RouteParameter $parentParam): self
-	{
-		$template = 'Parameter #%d ($%s) from parent request-handler "%s" is missing from child request-handler "%s", or is not in the same position.';
-
-		return new self(sprintf(
-			$template,
-			$parentParam->position,
-			$parentParam->name,
-			$parentRoute->requestHandler,
 			$childRoute->requestHandler
 		));
 	}
